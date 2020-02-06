27dfee24de7ab69835e3bb671372a0ca4224a5e5
Ganavigowda <ganavigowdal62587@gmail.com>
2020-02-06 17:43:38 +0530

    commiting 04-02-20

---
+++ 04-02-20/generic_collection2/generic_collection2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace generic_collectionSum
+{
+    class Animal
+    {
+        public string Name { get; set; }
+        public Animal(string name = "No Name")
+        {
+            Name = name;
+
+        }
+        public static void GetSum<T>(ref T num1, ref T num2)
+        {
+            double dblX = Convert.ToDouble(num1);
+            double dblY = Convert.ToDouble(num2);
+            Console.WriteLine($"{dblX}+{dblY}={dblX + dblY}");
+
+            int x = 5, y = 4;
+            Animal.GetSum(ref x, ref y);
+
+            string strX = "5", strY = "4";
+            Animal.GetSum(ref strX, ref strY);
+            Console.ReadLine();
+        }
+    }
+}
+       
+
+
+
